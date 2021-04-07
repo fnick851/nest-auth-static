edcc8ce75ed141aedbc5006c8b58126365c0f984
Noah <hanlun.song@gmail.com>
2021-04-07 02:15:47 -0400

    add static assets service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Response, Request } from 'express';
+import { join } from 'path';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  serveStatic(req: Request, res: Response) {
+    const { url } = req;
+    return res.sendFile(join(__dirname, '..', `assets${url}`));
   }
 }
